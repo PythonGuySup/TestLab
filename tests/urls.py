55ddfca5bd153374c96001338d010b30a0d5aba4
ShabalinTK <shabalin.timon@gmail.com>
2024-07-03 21:22:14 +0300

    описание проекта & сам тест

--- tests/urls.py
+++ tests/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from main import views
+from . import views
 
 urlpatterns = [
+    path("<int:test_id>/", views.test_detail, name="test_detail"),
+    path('<int:test_id>/execution/', views.test_questions, name='test_questions'),
 ]
--- tests/views.py
+++ tests/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Test, Question, Answer
 
-# Create your views here.
+def test_detail(request, test_id):
+    test = Test.objects.get(pk=test_id)
+    return render(request, 'test_detail.html', {'test': test})
+
+
+def test_questions(request, test_id):
+    test = Test.objects.get(id=test_id)
+    questions = Question.objects.filter(test=test)
+    answers = Answer.objects.filter(question__in=questions)
+
+    context = {
+        'test': test,
+        'questions': questions,
+        'answers': answers
+    }
+
+    return render(request, 'test_questions.html', context)
