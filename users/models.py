ddc4589f4fdb11afca2b438cd35b63eac255fb4c
ilshat <ilshat.yarullin.05@mail.ru>
2024-07-04 11:22:51 +0300

    Регистрация

--- users/models.py
+++ users/models.py
@@ -1,9 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
-
 
 # Create your models here.
-
-class UserDescription(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    description = models.TextField(max_length=1024, blank=True)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import RegistrationForm
+from main.views import home
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            messages.success(request, "Регистрация успешно завершена.")
+            return redirect(home)
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration.html', {'form': form})
