3925f6c22297125b9022d7f144ec3850f73b612b
ilshat <ilshat.yarullin.05@mail.ru>
2024-07-05 10:57:43 +0300

    updates

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from .forms import RegistrationForm
 from main.views import home_redirect
@@ -24,3 +25,15 @@ def register(request):
 def logout_view(request):
     logout(request)
     return redirect(home_redirect)
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect(home_redirect)
+        else:
+            messages.error(request, "Ошибка регистрации. Проверьте введенные данные.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
