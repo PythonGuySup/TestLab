1a6361e0635ad512950581e99162e15abdfc1b6e
PythonGuySup <egorkachanov2006@gmail.com>
2024-07-05 14:04:05 +0300

    Добавлен поиск

--- tests/views.py
+++ tests/views.py
@@ -1,13 +1,13 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from .models import Test, Question, Answer
 
 from django.db import transaction
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
 from tests.forms import ValidTest, ValidQuestion, ValidAnswer
-from tests.models import Test, Question, Answer, Category, User
+from tests.models import Test, Question, Category, User
 from datetime import datetime
 from json import JSONDecodeError, loads
+
 
 def test_detail(request, test_id):
     test = Test.objects.get(pk=test_id)
@@ -27,86 +27,83 @@ def test_questions(request, test_id):
     }
 
     return render(request, 'test_questions.html', context)
-  
+
 
 def create_test(test):
     test.instance.category = Category.objects.get(name=test.cleaned_data['category'])
-    user = test.cleaned_data['author'] # временное решение, потом будем получать из сеанса
-    
+    user = test.cleaned_data['author']  # временное решение, потом будем получать из сеанса
+
     test.instance.author = User.objects.get(username=user)
     test.instance.created_at = datetime.now()
     test.instance.updated_at = test.instance.created_at
-    
-    test.save()
-    
-def create_question(question, test):
 
+    test.save()
+
+
+def create_question(question, test):
     question.instance.test = test
     question.save()
 
 
-
 def create_answer(answer, question):
-    
     answer.instance.question = question
     answer.save()
 
-    
+
 @transaction.atomic
 def constructor_post(request):
     try:
 
         params_json = loads(request.body)
-            
+
         test_form = ValidTest(params_json)
-        
+
         if test_form.is_valid():
-            
+
             create_test(test_form)
-            
+
             for question_json in test_form.cleaned_data['questions'].values():
-                
+
                 question_form = ValidQuestion(question_json)
-                
+
                 if question_form.is_valid():
-                    
+
                     question = create_question(question_form, test_form.instance)
-                    
+
                     for answer_json in question_form.cleaned_data['answers'].values():
                         answer_form = ValidAnswer(answer_json)
                         if answer_form.is_valid():
-                            
-                            create_answer(answer_form, question_form.instance)    
+
+                            create_answer(answer_form, question_form.instance)
 
                         else:
                             transaction.set_rollback(True)
                             return HttpResponseBadRequest('invalid answer:' + str(answer_json))
-                        
+
                 else:
                     transaction.set_rollback(True)
                     return HttpResponseBadRequest('invalid question:' + str(question_json))
-            
+
         else:
             transaction.set_rollback(True)
             return HttpResponseBadRequest('invalid data:' + str(params_json))
-        
-        transaction.set_rollback(True) # отключил транзакцию для тестов
+
+        transaction.set_rollback(True)  # отключил транзакцию для тестов
         return HttpResponse("Test add sucsessfull")
-    
+
     except JSONDecodeError:
         transaction.set_rollback(True)
         return HttpResponseBadRequest('invalid stream params to JSON: ' + str(request.POST))
-    
+
     except Exception as E:
         transaction.set_rollback(True)
-        return HttpResponseServerError(E)            
-    
-    
+        return HttpResponseServerError(E)
+
 
 def constructor_get(resuest):
     return render(resuest, 'constructor.html')
-    
-    
+
+
 # Create your views here.
 
 def constructor(request):
@@ -114,7 +111,5 @@ def constructor(request):
         return constructor_post(request)
     elif request.method == 'GET':
         return constructor_get(request)
-    
-    # return render(request, 'get_tocken.html') получение токена для postman
-    
 
+    # return render(request, 'get_tocken.html') получение токена для postman
