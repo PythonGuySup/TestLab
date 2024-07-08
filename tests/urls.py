3e12faff957251f9c1c18f29b94d8788196780ca
ShabalinTK <shabalin.timon@gmail.com>
2024-07-08 10:45:41 +0300

    Сделаны страницы результата

--- tests/urls.py
+++ tests/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("<int:test_id>/", views.test_detail, name="test_detail"),
     path('<int:test_id>/execution/', views.test_questions, name='test_questions'),
     path('constructor/', views.constructor, name='constructor'),
-    path('constructor/<int:test_id>', views.constructor, name='reconstructor')
+    path('constructor/<int:test_id>', views.constructor, name='reconstructor'),
+    path('<int:test_id>/result/', views.test_result, name='test_result'),
 ]
